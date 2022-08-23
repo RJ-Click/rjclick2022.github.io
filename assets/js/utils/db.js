2adfe044c09d94afb95a1cf23097f831339036d6
Glaucio Daniel <glauciodaniel@gmail.com>
2022-08-23 19:24:44 -0300

    Ok

---
+++ assets/js/utils/db.js
@@ -0,0 +1,18 @@
+const categoria = [
+  "Museu",
+  "Ponto turístico",
+  "Galerias",
+  "Praças",
+  "Esportes",
+  "Restaurante",
+];
+
+const db = [
+  {
+    id: "1",
+    titulo: "visite o ",
+    destaque: "Rio",
+    subtitulo: "cidade maravilhosa",
+    categoria: 1,
+  },
+];
